90182033b20e64eb52b8fc8c48c0c2f7cd68ed21
Samuel <samucarimesg@hotmail.com>
2017-10-20 20:19:51 -0200

    qsd

---
+++ home.php
@@ -0,0 +1,93 @@
+<?php
+	$usuario = "root";
+	$senha = "";
+	$servidor = "localhost";
+	$bddnome = "cadastros";
+	$conexao = mysqli_connect($servidor,$usuario,$senha,$bddnome);
+	if(!$conexao){
+		echo "Sem conexao";
+	}
+	
+	session_start();
+	if(isset($_SESSION['usuario'])){
+		$id = $_SESSION['id'];
+		$unome = $_SESSION['usuario'];
+		$nome = mysqli_query ($conexao,"SELECT * FROM cadastro
+				WHERE id = $id");
+		$linha = mysqli_fetch_array($nome);
+		$name=$linha["nome"];
+		$sname = $linha["sobrenome"];
+	}
+	
+
+	if($_SERVER['REQUEST_METHOD']=="POST"){
+		$erro = false;
+		if(!file_exists('usuarios/'.$unome)){
+			mkdir('usuarios/'.$unome);	
+		}
+		if(isset($_FILES['perfil'])){
+			$nome_perfil= $_FILES['perfil']['name'];
+			$nome_perfil="perfil.jpg";
+			copy($_FILES['perfil']['tmp_name'],'usuarios/'.$unome.'/'. $nome_perfil);
+		}
+		if(isset($_FILES['painel'])){
+			$nome_painel= $_FILES['painel']['name'];
+			$nome_painel="painel.jpg";
+			copy($_FILES['painel']['tmp_name'],'usuarios/'.$unome.'/'. $nome_painel);
+		}
+	}
+		
+	?>
+
+<!doctype html>  
+<html>
+	<head>
+		<meta charset="utf-8"/>	
+		<link rel="stylesheet" href="./home.css"/>
+		<script src="./js/jquery.min.js"></script>
+		<script>
+			$(function(){
+				$(".perfil").attr("src","./usuarios/<?php echo $unome?>/perfil.jpg");
+				$(".painel").attr("src","./usuarios/<?php echo $unome?>/painel.jpg");
+				$(".aparecer").click(function(){
+					$(".fotos").css("display","block");
+				});
+				$(".enviar").click(function(){
+					$(".fotos").css("display","none");
+					$("body :not(.exc)").css("filter","none");
+				});
+			})
+		</script>
+	</head>
+	<body>
+		<?php
+		if(isset($_SESSION['usuario'])){
+		?>
+		<img src="./usuarios/<?php echo $unome; ?>/painel.jpg" class="capa"/>
+		<div class="info">
+			<img src="./usuarios/<?php echo $unome; ?>/perfil.jpg" class="perfil"/>
+			<h1 class="nome"> <?php echo $name." ". $sname?></h1>
+			<h4 class="nome"><?php echo $unome?></h4>
+		<?php
+			
+		?>
+			<button class="aparecer"> Trocar imagens </button>
+			<form action="home.php" method="post" enctype="multipart/form-data" class="fotos exc">
+				Escolha a foto de perfil<input name="perfil" type="file" value="Escolha a foto de perfil" class="exc"/><br>
+				Escolha o painel<input name="painel" type="file" value="Escolha a foto de painel" class="exc"/><br>
+				<input type="submit" class="enviar exc"/>
+			</form>
+		<?php
+			}
+		else{
+		?>
+			<p> Você ainda não está cadastrado </p>
+			<a href="login.php"> Fazer login </a>
+			
+		<?php
+		
+		}
+		?>
+	
+	</body>
+</html>
---
+++ login.php
@@ -0,0 +1,87 @@
+<!doctype html>  
+<html>
+	<head>
+		<meta charset="utf-8"/>	
+		<link rel="stylesheet" href="./css/login.css"/>
+		<link href="https://fonts.googleapis.com/css?family=Quicksand|Raleway" rel="stylesheet">
+		<script src="./js/jquery.min.js"></script>
+	</head>
+	<body>
+	<?php
+		if($_SERVER['REQUEST_METHOD']=="POST"){
+			$c = false;
+			$username=$_POST['usernome'];
+			$passw=$_POST['senha'];
+			
+			$usuario = "root";
+			$senha = "";
+			$servidor = "localhost";
+			$bddnome = "cadastros";
+			header('Content-Type: text/html, charset-utf-8');
+			$conexao = mysqli_connect($servidor,$usuario,$senha,$bddnome);
+			
+			if(!$conexao){
+				echo "Sem conexao";
+			}
+			$select = mysqli_query ($conexao,'SELECT * FROM cadastro');
+			$passw = hash("sha512",$passw);
+				while($linha = mysqli_fetch_array($select)){
+					if($linha["usernome"] == $username && $linha["senha"] == $passw){
+						$c = true;
+						$id = $linha["id"];
+						$username = $linha["usernome"];
+						break;
+					}
+				}
+				if($c == true){
+					if(isset($_SESSION['usuario'])){
+						session_destroy();
+						session_start();
+						$_SESSION['id'] = $id;
+						$_SESSION['usuario'] = $username;
+						print_r($_SESSION['usuario[1]']);
+						//header ("Location: home.php");
+					}
+					else{
+						session_start();
+						$_SESSION['id'] = $id;
+						$_SESSION['usuario'] = $username;
+						header ("Location: home.php");
+					}
+				}
+				else{
+					header ("Location: erro.php");
+				}
+				
+		}
+	?>
+		<h1> Entrar </h1>
+		
+		<?php
+			session_start();
+			if(!isset($_SESSION['usuario'])){
+		?>
+		<div class="login">
+		
+				<form method="post" action="login.php">
+					<input name='usernome' placeholder="Username" type="text"/><br/>
+					<input name= 'senha' placeholder="Senha" type="password"/><br/>
+					<input class="sub" type="submit" id="entrar" value="Entrar"/><br/>
+				</form>
+				<div class="botao">
+					<a class="entrar-cadastrar"  href="cadastrar.php"> Cadastre-se </a>
+				</div>
+			
+		</div>
+		<?php
+		}
+		else{
+			$usuario = $_SESSION['usuario'];
+			echo "Você já está cadastrado como $usuario <br/>";
+		?>
+			<a class="back_perfil" href="home.php">Ir para o perfil</a>
+		<?php
+		}
+		?>
+	</body>
+</html>
